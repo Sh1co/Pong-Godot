81a21a476e74c55c10a8e6f2cc178cf214e34bde
Sherif Nafee <shico.m.nafee@gmail.com>
2022-12-08 14:57:15 +0300

    Create goal

---
+++ Scripts/goal.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+
+signal goal_scored(goal_number)
+
+export var goal_number = 1
+
+var ball_group = "Ball"
+
+func _on_Goal_body_entered(body):
+	if body.is_in_group(ball_group):
+		body.queue_free()
+		emit_signal("goal_scored", goal_number)
