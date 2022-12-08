4d7b0116888ad9c46cd70d17c0f667f5466d883d
Sherif Nafee <shico.m.nafee@gmail.com>
2022-12-08 15:06:58 +0300

    Format code

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -8,31 +8,32 @@ export var winning_points = 7
 var p1_points = 0
 var p2_points = 0
 
+
 func _ready():
 	$HUD.show_message("Get ready!")
 	$BallSpawnDelay.start()
 	$Goal1.connect("goal_scored", self, "_point_scored")
 	$Goal2.connect("goal_scored", self, "_point_scored")
-	
+
+
 func _point_scored(goal):
 	if goal == 1:
-		p2_points+=1
+		p2_points += 1
 		$HUD.update_p2_score(p2_points)
-		if(p2_points==winning_points):
+		if p2_points == winning_points:
 			$HUD.show_message("P2 won!")
 			$GameEndDelay.start()
 			return
 		$HUD.show_message("P2 scored a point!")
 	elif goal == 2:
-		p1_points+=1
+		p1_points += 1
 		$HUD.update_p1_score(p1_points)
-		if(p1_points==winning_points):
+		if p1_points == winning_points:
 			$HUD.show_message("P1 won!")
 			$GameEndDelay.start()
 			return
 		$HUD.show_message("P1 scored a point!")
 	$BallSpawnDelay.start()
-	
 
 
 func _spawn_ball():
@@ -40,6 +41,7 @@ func _spawn_ball():
 	var ball = ball_ps.instance()
 	ball.position = $BallSpawnPosition.position
 	add_child(ball)
-	
+
+
 func end_game():
 	emit_signal("game_ended")
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,16 +1,12 @@
 extends RigidBody2D
 
-enum PlayerType {
-	Player1,
-	Player2,
-	CPU
-}
-
+enum PlayerType { Player1, Player2, CPU }
 
 export(PlayerType) var player_type = PlayerType.Player1
 export var speed = 200
 
 var player_code = ""
+
 
 func _ready():
 	if player_type == PlayerType.Player1:
@@ -18,10 +14,12 @@ func _ready():
 	elif player_type == PlayerType.Player2:
 		player_code = "p2"
 
+
 var velocity = linear_velocity
 
+
 func _integrate_forces(state):
-	if player_code!="":
+	if player_code != "":
 		velocity = Vector2.ZERO
 		if Input.is_action_pressed(player_code + "_up"):
 			velocity = Vector2(0, -speed)
